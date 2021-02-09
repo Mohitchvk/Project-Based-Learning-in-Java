51f140c48f430a6b530cf41e7c5d838c161bfc74
Mohitchvk <mohit.chvk@gmail.com>
2021-02-09 19:26:46 +0530

    Added Expirement One

---
+++ src/expirementOne/EmployeeData.java
@@ -0,0 +1,13 @@
+package expirementOne;
+
+public class EmployeeData {
+    int eid;
+    String name;
+    long phno;
+
+    EmployeeData(int eid, String name, long phno) {
+        this.eid=eid;
+        this.name=name;
+        this.phno=phno;
+    }
+}
